e413b09480a0defc60213fba04ec441000fe0cac
Priya-DP <priyavenkatesan41@gmail.com>
2025-01-09 06:44:15 -0800

    Initial ROuting setup

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 const express = require("express");
-
+const path = require("path");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 
 app.get("/", (req, res) => {
   res.json({ mgs: "Hello world" });
 });
 
 
-app.listen(8000, ()=>{
-    console.log(`server running successfully in the port :  http://localhost : ${8000}`);
-    
+// CRUD Functionality
+
+// R- Reading (View)
+app.get('/movies', ()=>{
+
 })
+
+// c - Create
+app.post('/movies', ()=>{
+
+})
+
+
+//  U - update
+app.put('/movies/:id',()=>{
+
+})
+
+// D -Delete
+app.delete('/movie/:id',()=>{
+
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server listening to the port ${process.env.PORT} in ${process.env.NODE_ENV} `
+  );
+});
